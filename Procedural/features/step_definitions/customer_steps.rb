31b27c02863f3a429937c21832fb511a148f290f
Ethan Dennis <erdennis13@gmail.com>
2015-11-09 11:09:55 -0800

    Added screenshots to customer steps

--- Procedural/features/step_definitions/customer_steps.rb
+++ Procedural/features/step_definitions/customer_steps.rb
@@ -1,12 +1,17 @@
 When(/^I navigate to "(.*?)" tab$/) do |arg1|
 	wait_for_element_exists "* marked:'6-Week Sales'"
+	screenshot name: "About to tap on #{arg1} tab"
 	touch "android.widget.TextView text:'#{arg1}'"
+	screenshot name: "Tapped on #{arg1} tab"
 end
 
 When(/^I click on the first contact$/) do
+	screenshot name: "About to tap on first contact"
 	touch "ViewCellRendererViewCellContainer index:0"
+	screenshot name: "Tapped on first contact"
 end
 
 Then(/^I should see "(.*?)" present on screen$/) do |arg1|
 	wait_for_element_exists "* marked:'#{arg1}'"
+	screenshot name: "Verified that #{arg1} is present"
 end
